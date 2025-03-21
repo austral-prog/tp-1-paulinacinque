6696a1bd5bbdbdd505d48a11cf776645ecab1a8a
paulinacinque <pcinque1@mail.austral.edu.ar>
2025-03-21 12:22:52 -0300

    Update exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,14 @@
 def math():
     a = 57
     b = 7
+
+    print(a+b)
+    print(a-b)
+    print(a*b)
+    print((a+b)/2)
+    print(a//b)
+    print(a%b)
+    print(a/b)
+
+
+#escribir con sangría para que esté "adentro" de la función
